4f3834930cc67e7806ba6840d2e5bda6019ef337
Jeff Astor <Jeff.X.Astor.-ND@disney.com>
2021-05-26 00:28:37 -0700

    Starting on advanced version.

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,57 @@
+import { Link } from "react-router-dom"
+import apiClient from "../../services/apiClient"
+import Twitter from "../Icons/Twitter"
+import Instagram from "../Icons/Instagram"
+import Facebook from "../Icons/Facebook"
+import "./Navbar.css"
+
+export default function Navbar({ user, setUser }) {
+  const handleLogout = async () => {
+    await apiClient.logoutUser()
+    setUser({})
+  }
+
+  return (
+    <nav className="Navbar">
+      <div className="content">
+        <div className="logo">
+          <Link to="/">
+            <h1>RATE MY SETUP</h1>
+          </Link>
+        </div>
+
+        <div className="socials">
+          <Twitter fill="var(--pure-white)" />
+          <Instagram fill="var(--pure-white)" />
+          <Facebook fill="var(--pure-white)" />
+        </div>
+
+        <ul className="links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          {user?.email ? (
+            <>
+              <li>
+                <span>{user.email}</span>
+              </li>
+
+              <li>
+                <span onClick={handleLogout}>Logout</span>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/register">Sign Up</Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
+    </nav>
+  )
+}
